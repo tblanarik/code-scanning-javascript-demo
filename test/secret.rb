fc0318c074bb758f4e60b235b5fc2b821a32a41e
Trevor Blanarik <tblanarik@github.com>
2021-11-03 21:02:04 -0700

    Create secret.rb

---
+++ test/secret.rb
@@ -0,0 +1,36 @@
+require 'openssl'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'base64'
+
+payload = <<-EOL
+[{"token":"some_token","type":"some_type","url":"some_url"}]
+EOL
+
+payload = payload
+
+signature = "MEUCIQDKZokqnCjrRtw0tni+2Ltvl/uiMJ1EGumEsp1BsNr32AIgQY1YXD2nlj+XNfGK4rBfkMJ1JDOQcYXxa2sY8FNkrKc="
+
+key_id = "90a421169f0a406205f1563a953312f0be898d3c7b6c06b681aa86a874555f4a"
+
+url = URI.parse('https://api.github.com/meta/public_keys/secret_scanning')
+
+raise "Need to define GITHUB_PRODUCTION_TOKEN environment variable" unless ENV['GITHUB_PRODUCTION_TOKEN']
+request = Net::HTTP::Get.new(url.path)
+request['Authorization'] = "Bearer #{ENV['GITHUB_PRODUCTION_TOKEN']}"
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = (url.scheme == "https")
+
+response = http.request(request)
+
+parsed_response = JSON.parse(response.body)
+
+current_key_object = parsed_response["public_keys"].find { |key| key["key_identifier"] == key_id }
+
+current_key = current_key_object["key"]
+
+openssl_key = OpenSSL::PKey::EC.new(current_key)
+
+puts openssl_key.verify(OpenSSL::Digest::SHA256.new, Base64.decode64(signature), payload.chomp)
